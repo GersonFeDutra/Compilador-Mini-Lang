3ef7a1ebe9da5a57f0cb2c43275405fceb4b329f
GersonFeDutra <gersonfedutra@gmail.com>
2026-02-13 17:51:35 -0300

    ide: add workspace recommendations

--- code.ml
+++ code.ml
@@ -1,5 +1,5 @@
 #!./lexer.py
-// -*- mode: c++ -*-
+// -*- mode: cpp -*-
 // vim: set filetype=c++:
 
 // TODO
@@ -19,43 +19,41 @@ set resultado = calcular ( x ) ;
 print resultado ;
 */
 
+
+// Current
+z : bool
+
+a, b : int  # single-line multiple declarations
+
+# old.TODO -> single-line multiple assignments
+#a, b = 1, 2
+
+/* /* /*/ */* */ */ # nested comments
+
+# old.TODO -> Captures
+# [a]
 {
-    
-    z : bool
+    # single-line multiple expressions
+    b : bool; a : bool; b = 2 + 3
 
-    a, b : int  # single-line multiple declarations
-    
-    # TODO -> single-line multiple assignments
-    #a, b = 1, 2
+    a = 3
+    + 2
 
-    /* /* /*/ */* */ */ # nested comments
-
-    # TODO -> Captures
-    # [a]
-    {
-        # single-line multiple expressions
-        b : bool; a : bool; b = 2 + 3
-
-        a = 3
+    # line continuation
+    a = 3\
         + 2
-
-        # line continuation
-        a = 3\
-            + 2
-        
-        # * 4 - 5 / 6
-        /* 7 : char */ # ERROR
-    }
     
-    /*
-    for (i := 0; i < 10; i ++)
-    {
-        cout << 10 << " "
-    }
-    */
-    
-    #< Also supports this format.
-    Could be used as annotation-blocks in the future.
-    >#
-
+    # * 4 - 5 / 6
+    /* 7 : char */ # ERROR
 }
+
+/* old.TODO
+for (i := 0; i < 10; i ++)
+{
+    cout << 10 << " "
+}
+*/
+
+#< Also supports this format.
+Could be used as annotation-blocks in the future.
+>#
